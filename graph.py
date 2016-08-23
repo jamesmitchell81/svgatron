51f654e0cb7b3414f9ac7391feba3e9a562318cd
jamesmitchell81 <james.mitchell81@live.co.uk>
2016-08-23 07:54:21 +0100

    graph added

---
+++ graph.py
@@ -0,0 +1,38 @@
+from elements import Line, Text, Transform
+
+class Graph(object):
+  self._margin = {
+    "top"   : 0,
+    "left"  : 0,
+    "right" : 0,
+    "bottom": 0
+  }
+
+  self.x_baseline = 0
+  self.y_baseline = 0
+  self.tick_length = 5
+
+  def __init__(self, width, height):
+    self._width = width
+    self._height = height
+
+  def __init__(self, data = []):
+    print "j"
+
+  @property
+  def width(self):
+    return self._width
+
+  @width.setter
+  def width(self, width):
+    self._width = width
+
+  @property
+  def height(self):
+    return self._height
+
+  @width.setter
+  def height(self, height):
+    self._height = height
+
+  def x_axis(self, ):
